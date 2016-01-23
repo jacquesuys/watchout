fdec0adfa93503cd2fcc714bf3ba5699001dc870
jacquesuys <jaxuys@gmail.com>
2016-01-23 15:27:54 -0800

    Full screen mode.

--- client/watchout.js
+++ client/watchout.js
@@ -3,8 +3,9 @@
   // css margin
   var margin = {top: 20, right: 20, bottom: 20, left: 20};
   // returns margin object value
-  var w = 500 - margin.left - margin.right,
-      h = 400 - margin.top - margin.bottom;
+  // Create the width and height
+  var w = window.innerWidth - margin.left - margin.right,
+      h = window.innerHeight - margin.top - margin.bottom;
 
   var enemies = [];
 
@@ -13,13 +14,14 @@
     this.y = y;
     this.r = r;
   };
-
+  // Enemy constructor
   var Enemy = function () {
     var ran1 = Math.floor(Math.random() * w);
     var ran2 = Math.floor(Math.random() * h);
+    // Call the superclass with this Enemy, random width, random height, and radius
     Unit.call(this, ran1, ran2, 20);
   };
-
+  // produce enemy circles
   var enemyMaker = function(n) {
     while (n) {
       var enemy = new Enemy();
@@ -45,8 +47,8 @@
   };
 
   var dragged = function(d) {
-    d3.select(this)
-      .attr({ "transform": "translate(" + window.event.clientX + "," + window.event.clientY + ")"});
+    d3.select('.hero')
+      .attr({ cx: window.event.clientX, cy: window.event.clientY});
   };
 
   var drag = d3.behavior.drag();
@@ -62,6 +64,25 @@
   .append('g')
   .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
+  // Create custom tween (animation term) for each enemies created
+  var enemyTween  = setInterval(function() {
+    _.each(enemies, function (datum) {
+        datum.x = Math.round(Math.random() * w);
+        datum.y = Math.round(Math.random() * h);
+    });
+
+    svg.selectAll('.enemy')
+    .transition()
+    .duration(1000)
+    .attr('cx', function (d) {
+        return d.x;
+    })
+    .attr('cy', function (d) {
+        return d.y;
+    });
+  }, 1000);
+  
+  // selects the circles in the board, use data to append the circles with the attribute of cx, cy, and r.
   svg.selectAll('circle')
   .data([player])
   .enter()
@@ -78,26 +99,7 @@
       return d.r;
   })
   .call(drag);
-
-  // Create custom tween (animation term) for each enemies created
-  var enemyTween  = setInterval(function() {
-    _.each(enemies, function (datum) {
-        datum.x = Math.round(Math.random() * 500);
-        datum.y = Math.round(Math.random() * 400);
-    });
-
-    svg.selectAll('.enemy')
-    .transition()
-    .duration(1000)
-    .attr('cx', function (d) {
-        return d.x;
-    })
-    .attr('cy', function (d) {
-        return d.y;
-    });
-  }, 1000);
   
-  // selects the circles in the board, use data to append the circles with the attribute of cx, cy, and r.
   svg.selectAll('circle')
   .data(enemies)
   .enter()
@@ -115,19 +117,4 @@
   })
   .transition()
   .tween(enemyTween);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
