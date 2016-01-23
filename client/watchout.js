ed519c63ae28d66f310f0de8881483c0e7b850b5
jacquesuys <jaxuys@gmail.com>
2016-01-22 19:40:19 -0800

    Added circles SVG with x and y coordinate, score counter function, superclass and subclass called enemy.

--- client/watchout.js
+++ client/watchout.js
@@ -1,36 +1,99 @@
 // start slingin' some d3 here.
 
 
-// Params
-var gameOptions = {
-  height: 450,
-  width: 700,
-  nEnemies: 30,
-  padding: 20
-};
+  // General game options
+  var gameOptions = {
+    height: 450,
+    width: 700,
+    nEnemies: 30,
+    padding: 20
+  };
 
-var enemies = [1, 2, 3];
+  // Store the enemies in the storage array
+  var enemies = [];
+
+  // Psuedoclassical approach
+  var Unit = function (x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+  };
+
+
+  // Move unit for all the enemies
+  // Unit.prototype.move = function() {
+  //   var randY = Math.floor(0, Math.random() * gameOptions.height);
+  //   var randX = Math.floor(0, Math.random() * gameOptions.width);
+
+  // ;(setInterval(function() {
+  //     this.x = randX;
+  //     this.y = randY;
+  //   }, 1000))();  
+  // };
+
+  var i = 3;
+  while(i > 0) {
+    var Enemy = new Unit(50, 50, 40);
+    enemies.push(Enemy);
+    i--;
+  }
 
 // Scores
-
-// Board config
-  // find container
+  // Board config
+    // find container
   var $board = d3.select('.board');
     
-    $board
-    .style('background-color', 'black')
-    .style('height', gameOptions.height + 'px')
-    .style('width', gameOptions.width + 'px')
-    .style('padding', gameOptions.padding + 'px');    
+  $board
+  .style('background-color', 'black')
+  .style('height', gameOptions.height + 'px')
+  .style('width', gameOptions.width + 'px')
+  .style('padding', gameOptions.padding + 'px');    
   // append SVG scalable vector graphic
     // add attr
 
-    $board
-    .selectAll('.board')
-    .data(enemies)
-    .enter()
-    .append('svg')
-    .attr('class', 'enemies');
+  // Select the board, 
+  $board
+  .selectAll('.board')
+  .data(enemies)
+  .enter()
+  .append('svg')
+  .attr('class', 'enemy')
+  .append('circle')
+  .attr('cx', function(d){
+    return d.x + 'px';
+  })
+  .attr('cy', function(d){
+    return d.y + 'px';
+  })
+  .attr('cr', function (d) {
+    return d.r + 'px';
+  });
+  
+
+  // d3.selectAll('.enemy')
+  //   .transition()
+  //   .duration(500)
+  //   .attr('x', function (d) { // return object and return property x
+  //     return d.x;
+  //   })
+  //   .attr('y', function (d) { // return the object and return property x;
+  //     return d.y;
+  //   })
+
+
+    
+  function increment () {
+    var i = 0; 
+    return function () {
+      i++;
+      d3.select('.current span')
+        .text(i);
+      d3.select('.highscore span')
+        .text(i);
+    };
+  };
+
+  setInterval(increment(), 100);
 
 // Update score
   // html span
